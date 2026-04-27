255bb9b6e021dc853c93ef656e2eeb9f920f1bd4
Gabriele Di Muccio <gabrieledimuccio@gmail.com>
2026-04-27 17:32:36 +0200

    terminato selfwork PHP2

--- index2.php
+++ index2.php
@@ -1,3 +1,12 @@
 <?php
+$text1 = "Marco";
+$text2 = "hai";
+$text3 = "sete";
+$text4 = "?";
+$text5 = "Perchè";
+$text6 = $text2;
+$text7 = "bevuto";
+$text8 = "tutto";
 
+echo $text1 . " " . $text2 . " " . $text3 . $text4 . " " . $text5 . " " . $text6 . " " . $text7 . " " . $text8;
 ?>
